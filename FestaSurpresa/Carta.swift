b5ed25c92db82d61458ff3c151554e1f4d2501bc
mateus nunes <mateusnunesnunes487@gmail.com>
2019-11-26 14:09:08 -0300

    Criação singleton-Model
    criação classe carta, e as cartas do jogo

---
+++ FestaSurpresa/Carta.swift
@@ -0,0 +1,9 @@
+//
+//  Cartas.swift
+//  FestaSurpresa
+//
+//  Created by Mateus Nunes on 26/11/19.
+//  Copyright © 2019 Aoba. All rights reserved.
+//
+
+import Foundation
---
+++ FestaSurpresa/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  FestaSurpresa
+//
+//  Created by Mateus Nunes on 26/11/19.
+//  Copyright © 2019 Aoba. All rights reserved.
+//
+
+import Foundation
