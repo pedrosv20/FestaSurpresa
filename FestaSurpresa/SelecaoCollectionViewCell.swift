3bd7e25a991de0dafd42b0e5ab4c68ffc859f714
Matheus Cavasin <matheus.scavasin@gmail.com>
2019-11-27 08:56:38 -0300

    Collection view adicionada
    primeiros passos com a collection colocada

---
+++ FestaSurpresa/SelecaoCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SelecaoCollectionViewCell.swift
+//  FestaSurpresa
+//
+//  Created by MCavasin on 26/11/19.
+//  Copyright © 2019 Aoba. All rights reserved.
+//
+
+import UIKit
+
+class SelecaoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var nomePlayer: UILabel!
+    
+}
