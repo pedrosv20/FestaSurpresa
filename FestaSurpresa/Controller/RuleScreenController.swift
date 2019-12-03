722c5eaeadef67746ae72611737c4f80246755a0
mateus nunes <mateusnunesnunes487@gmail.com>
2019-12-03 10:48:19 -0300

    Rule Screen 2
    Rule Screen 2

--- FestaSurpresa/Controller/RuleScreenController.swift
+++ FestaSurpresa/Controller/RuleScreenController.swift
@@ -6,13 +6,15 @@ class RuleScreenController: UIViewController {
     
     @IBOutlet var mainView: UIView!
     
+    @IBOutlet weak var btnPersonagens: UIButton!
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var llbTituloPoder: UILabel!
     @IBOutlet weak var lblPoder: UILabel!
     @IBOutlet weak var viewSk: SKView!
-    
+    @IBOutlet weak var btnInstrucoes: UIButton!
+    @IBOutlet weak var btnSair: UIButton!
     @IBOutlet weak var lblNomeJogador: UILabel!
-    
     @IBOutlet weak var lblDescricao: UILabel!
-
     @IBOutlet weak var btnDistraido: UIButton!
     @IBOutlet weak var btnParceiro: UIButton!
     @IBOutlet weak var btnPai: UIButton!
@@ -21,45 +23,132 @@ class RuleScreenController: UIViewController {
     @IBOutlet weak var btnPenetra: UIButton!
     @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var btnPalhaco: UIButton!
+    var escolhido = 0
+    
+    @IBAction func actBtnPersonagens(_ sender: Any) {
+        btnInstrucoes.layer.borderWidth = 1
+        btnInstrucoes.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        btnInstrucoes.backgroundColor = .clear
+        btnInstrucoes.setTitleColor(.black, for: .normal)
+        btnPersonagens.backgroundColor = .black
+        btnPersonagens.setTitleColor(.white, for: .normal)
+        mostrar()
+    }
+    
     
     @IBAction func actBtnPalhaco(_ sender: Any) {
+        mostrar()
+        escolhido = 7
         lblNomeJogador.text = Model.shared.cartas[7].nome
         lblDescricao.text = Model.shared.cartas[7].descricao
         lblPoder.text = Model.shared.cartas[7].poder
     }
     @IBAction func actBtnPenetra(_ sender: Any) {
+        mostrar()
+        escolhido = 2
         lblNomeJogador.text = Model.shared.cartas[2].nome
         lblDescricao.text = Model.shared.cartas[2].descricao
         lblPoder.text = Model.shared.cartas[2].poder
     }
     @IBAction func actBtnBebado(_ sender: Any) {
+        mostrar()
+        escolhido = 4
         lblNomeJogador.text = Model.shared.cartas[4].nome
         lblDescricao.text = Model.shared.cartas[4].descricao
         lblPoder.text = Model.shared.cartas[4].poder
     }
     
     @IBAction func actBtnDistraido(_ sender: Any) {
+        mostrar()
+        escolhido = 5
          lblNomeJogador.text = Model.shared.cartas[5].nome
-               lblDescricao.text = Model.shared.cartas[5].descricao
+        lblDescricao.text = Model.shared.cartas[5].descricao
         lblPoder.text = Model.shared.cartas[5].poder
     }
     @IBAction func actBtnNamorado(_ sender: Any) {
+        mostrar()
+        escolhido = 0
         lblNomeJogador.text = Model.shared.cartas[0].nome
         lblDescricao.text = Model.shared.cartas[0].descricao
         lblPoder.text = Model.shared.cartas[0].poder
     }
     @IBAction func actBtnPai(_ sender: Any) {
+        mostrar()
+        escolhido = 3
         lblNomeJogador.text = Model.shared.cartas[3].nome
         lblDescricao.text = Model.shared.cartas[3].descricao
         lblPoder.text = Model.shared.cartas[3].poder
     }
     
+    @IBAction func actBtnSair(_ sender: Any) {
+        mostrar()
+    }
     @IBAction func actBtnParceiro(_ sender: Any) {
+        mostrar()
+        escolhido = 1
          lblNomeJogador.text = Model.shared.cartas[1].nome
          lblDescricao.text = Model.shared.cartas[1].descricao
         lblPoder.text = Model.shared.cartas[1].poder
     }
-    
+    func mostrar(){
+        
+        btnInstrucoes.layer.borderWidth = 1
+        btnInstrucoes.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        btnInstrucoes.backgroundColor = .clear
+        btnInstrucoes.setTitleColor(.black, for: .normal)
+        btnPersonagens.backgroundColor = .black
+        btnPersonagens.setTitleColor(.white, for: .normal)
+        UIView.animate(withDuration: 0.3, delay: 0, options: [], animations: {
+            
+            self.btnSair.alpha = 0
+            self.stackView.alpha = 1
+            self.lblDescricao.alpha = 1
+            self.lblPoder.alpha = 1
+            self.lblNomeJogador.alpha = 1
+            self.llbTituloPoder.alpha = 1
+            
+        }, completion: { _ in
+           self.stackView.isHidden = false
+            self.lblDescricao.isHidden = false
+            self.lblPoder.isHidden = false
+            self.lblNomeJogador.isHidden = false
+            self.llbTituloPoder.isHidden = false
+            self.lblNomeJogador.text = Model.shared.cartas[self.escolhido].nome
+            self.lblDescricao.text = Model.shared.cartas[self.escolhido].descricao
+            self.lblNomeJogador.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0)
+        })
+    }
+    @IBAction func actBtnInstrucao(_ sender: Any) {
+        btnPersonagens.layer.borderWidth = 1
+        btnPersonagens.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        btnInstrucoes.backgroundColor = .black
+        btnInstrucoes.setTitleColor(.white, for: .normal)
+        btnPersonagens.backgroundColor = .clear
+        btnPersonagens.setTitleColor(.black, for: .normal)
+        UIView.animate(withDuration: 0.2, delay: 0, options: [], animations: {
+            self.btnSair.alpha = 1
+            self.stackView.alpha = 0
+            self.lblDescricao.alpha = 0
+            self.lblPoder.alpha = 0
+//            self.lblNomeJogador.alpha = 0
+            self.llbTituloPoder.alpha = 0
+            
+        }, completion: { _ in
+            self.btnSair.isHidden = false
+            self.stackView.isHidden = true
+            self.lblDescricao.isHidden = true
+            self.lblPoder.isHidden = true
+//            self.lblNomeJogador.isHidden = true
+            self.llbTituloPoder.isHidden = true
+            self.lblNomeJogador.text = "Instruções"
+            self.lblNomeJogador.layer.borderWidth = 2
+            self.lblNomeJogador.layer.cornerRadius = 5
+            self.lblNomeJogador.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            self.lblDescricao.text = Model.shared.instrucao[0]
+            
+        })
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -70,7 +159,7 @@ class RuleScreenController: UIViewController {
             scene.scaleMode = .aspectFill
             viewSk.presentScene(scene)
         }
-        
+       
         mainView.backgroundColor = UIColor.clear
         let colors = Colors()
         let backgroundLayer = colors.gl
@@ -78,5 +167,14 @@ class RuleScreenController: UIViewController {
         mainView.layer.insertSublayer(backgroundLayer!, at: 0)
         contentView.layer.cornerRadius = 15.0
         contentView.alpha = 0.8
+        btnSair.layer.cornerRadius = 3
+        btnSair.isHidden = true
+        btnInstrucoes.layer.cornerRadius = 3
+        btnPersonagens.layer.cornerRadius = 3
+        btnInstrucoes.setTitleColor(.black, for: .normal)
+        btnInstrucoes.layer.borderWidth = 1
+        btnInstrucoes.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        btnPersonagens.setTitleColor(.white, for: .normal)
+        btnPersonagens.backgroundColor = .black
     }
 }
