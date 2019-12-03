6f1a9869eb079fd8a4cb4e68bac2b1f1db6985aa
mateus nunes <mateusnunesnunes487@gmail.com>
2019-12-03 11:41:08 -0300

    RuleScreen 3
    RuleScreen 3

--- FestaSurpresa/Controller/RuleScreenController.swift
+++ FestaSurpresa/Controller/RuleScreenController.swift
@@ -6,6 +6,7 @@ class RuleScreenController: UIViewController {
     
     @IBOutlet var mainView: UIView!
     
+    @IBOutlet weak var stackMenu: UIStackView!
     @IBOutlet weak var btnPersonagens: UIButton!
     @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var llbTituloPoder: UILabel!
@@ -23,9 +24,71 @@ class RuleScreenController: UIViewController {
     @IBOutlet weak var btnPenetra: UIButton!
     @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var btnPalhaco: UIButton!
+    @IBOutlet weak var btnMenuJogador: UIButton!
+    @IBOutlet weak var btnMenuEtapas: UIButton!
+    @IBOutlet weak var btnMenuCasos: UIButton!
+    @IBOutlet weak var btnMenuRodadas: UIButton!
+    @IBOutlet weak var btnMenuLider: UIButton!
+    @IBOutlet weak var btnMenuFinal: UIButton!
+    
+    
+    
     var escolhido = 0
     
+    func esconder(esco:Int){
+        lblDescricao.font = lblDescricao.font.withSize(25)
+        UIView.animate(withDuration: 0.2, delay: 0, options: [], animations: {
+                    self.btnSair.alpha = 1
+                    self.stackView.alpha = 0
+                    self.lblDescricao.alpha = 1
+                    self.lblPoder.alpha = 0
+        //            self.lblNomeJogador.alpha = 0
+                    self.llbTituloPoder.alpha = 0
+                    self.stackMenu.alpha = 0
+                    
+                }, completion: { _ in
+                    self.stackMenu.isHidden = true
+                    self.btnSair.isHidden = false
+                    self.stackView.isHidden = true
+                    self.lblDescricao.isHidden = false
+                    self.lblPoder.isHidden = true
+        //            self.lblNomeJogador.isHidden = true
+                    self.llbTituloPoder.isHidden = true
+                    self.lblNomeJogador.text = "Instruções"
+                    self.stackMenu.isHidden = false
+                    self.lblNomeJogador.layer.borderWidth = 2
+                    self.lblNomeJogador.layer.cornerRadius = 5
+                    self.lblNomeJogador.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+                    self.lblDescricao.text = Model.shared.instrucao[esco]
+                    
+                })
+    }
+    
+    @IBAction func actBtnMenuJogador(_ sender: Any) {
+        print("jogador")
+        esconder(esco: 0)
+    }
+    @IBAction func actBtnMenuEtapa(_ sender: Any) {
+         esconder(esco: 1)
+    }
+    @IBAction func actBtnMenuRodadas(_ sender: Any) {
+         esconder(esco: 2)
+    }
+    @IBAction func actBtnMenuCasos(_ sender: Any) {
+         esconder(esco: 3)
+    }
+    @IBAction func actBtnMenuLider(_ sender: Any) {
+         esconder(esco: 4)
+        lblDescricao.font = lblDescricao.font.withSize(18)
+    }
+    @IBAction func actBtnMenuFinal(_ sender: Any) {
+         esconder(esco: 5)
+    }
+    
+    
+    
     @IBAction func actBtnPersonagens(_ sender: Any) {
+        
         btnInstrucoes.layer.borderWidth = 1
         btnInstrucoes.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         btnInstrucoes.backgroundColor = .clear
@@ -91,7 +154,6 @@ class RuleScreenController: UIViewController {
         lblPoder.text = Model.shared.cartas[1].poder
     }
     func mostrar(){
-        
         btnInstrucoes.layer.borderWidth = 1
         btnInstrucoes.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         btnInstrucoes.backgroundColor = .clear
@@ -99,7 +161,7 @@ class RuleScreenController: UIViewController {
         btnPersonagens.backgroundColor = .black
         btnPersonagens.setTitleColor(.white, for: .normal)
         UIView.animate(withDuration: 0.3, delay: 0, options: [], animations: {
-            
+            self.stackMenu.alpha = 0
             self.btnSair.alpha = 0
             self.stackView.alpha = 1
             self.lblDescricao.alpha = 1
@@ -108,14 +170,18 @@ class RuleScreenController: UIViewController {
             self.llbTituloPoder.alpha = 1
             
         }, completion: { _ in
+            self.stackMenu.isHidden = true
            self.stackView.isHidden = false
             self.lblDescricao.isHidden = false
             self.lblPoder.isHidden = false
+            self.stackMenu.isHidden = false
             self.lblNomeJogador.isHidden = false
             self.llbTituloPoder.isHidden = false
             self.lblNomeJogador.text = Model.shared.cartas[self.escolhido].nome
             self.lblDescricao.text = Model.shared.cartas[self.escolhido].descricao
             self.lblNomeJogador.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0)
+            self.lblDescricao.font = self.lblDescricao.font.withSize(25)
+
         })
     }
     @IBAction func actBtnInstrucao(_ sender: Any) {
@@ -126,21 +192,23 @@ class RuleScreenController: UIViewController {
         btnPersonagens.backgroundColor = .clear
         btnPersonagens.setTitleColor(.black, for: .normal)
         UIView.animate(withDuration: 0.2, delay: 0, options: [], animations: {
-            self.btnSair.alpha = 1
+//            self.btnSair.alpha = 1
             self.stackView.alpha = 0
             self.lblDescricao.alpha = 0
             self.lblPoder.alpha = 0
 //            self.lblNomeJogador.alpha = 0
             self.llbTituloPoder.alpha = 0
+            self.stackMenu.alpha = 1
             
         }, completion: { _ in
-            self.btnSair.isHidden = false
+//            self.btnSair.isHidden = false
             self.stackView.isHidden = true
             self.lblDescricao.isHidden = true
             self.lblPoder.isHidden = true
 //            self.lblNomeJogador.isHidden = true
             self.llbTituloPoder.isHidden = true
             self.lblNomeJogador.text = "Instruções"
+            self.stackMenu.isHidden = false
             self.lblNomeJogador.layer.borderWidth = 2
             self.lblNomeJogador.layer.cornerRadius = 5
             self.lblNomeJogador.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
@@ -159,7 +227,7 @@ class RuleScreenController: UIViewController {
             scene.scaleMode = .aspectFill
             viewSk.presentScene(scene)
         }
-       
+        stackMenu.isHidden = true
         mainView.backgroundColor = UIColor.clear
         let colors = Colors()
         let backgroundLayer = colors.gl
@@ -176,5 +244,6 @@ class RuleScreenController: UIViewController {
         btnInstrucoes.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         btnPersonagens.setTitleColor(.white, for: .normal)
         btnPersonagens.backgroundColor = .black
+        btnSair.isHidden = true
     }
 }
