88b480176ab0c367873911b474c6a878dccc1e4f
mateus nunes <mateusnunesnunes487@gmail.com>
2019-12-05 13:26:33 -0300

    s

--- FestaSurpresa/Controller/CardViewController.swift
+++ FestaSurpresa/Controller/CardViewController.swift
@@ -52,7 +52,23 @@ class CardViewController: UIViewController {
 //        cardView.layer.borderColor = UIColor.black.cgColor
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        
+        roleTitleLabel.text = SessionHandler.shared.cardTouched!.nome
+        descricao.text = SessionHandler.shared.cardTouched!.descricao
+        poder.text = SessionHandler.shared.cardTouched!.poder
+        cardIcon.image = SessionHandler.shared.cardTouched!.icone
+    }
     @IBAction func didPressHideCardButton(_ sender: Any) {
+        if SessionHandler.shared.sawCard == true {
+            //TODO: manda mensagfem de ok
+        }
+        self.dismiss(animated: false, completion: nil)
+//        print(Model.shared.players.first?.carta?.nome)
+//        print(Model.shared.players.count)
+//        for i in Model.shared.players {
+//            print(i.nome)
+//        }
     }
     
     
