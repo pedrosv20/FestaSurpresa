1cde0bb52f06179a174f9d5fa8dd767f75c1d0df
Pedro <pedro1silva1vargas@gmail.com>
2019-12-05 14:14:40 -0300

    Pedro finnaly working

--- FestaSurpresa/Model/Colors.swift
+++ FestaSurpresa/Model/Colors.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+import UIKit
+class Colors {
+    var gl:CAGradientLayer!
+
+    init() {
+        let colorTop = UIColor(red: 217.0 / 255.0, green: 196.0 / 255.0, blue: 102.0 / 255.0, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 229.0 / 255.0, green: 188.0 / 255.0, blue: 76.0 / 255.0, alpha: 1.0).cgColor
+
+        self.gl = CAGradientLayer()
+        self.gl.colors = [colorTop, colorBottom]
+        self.gl.locations = [0.0, 1.0]
+    }
+}
